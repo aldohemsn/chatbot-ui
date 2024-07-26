59547484c74c4ebc2a598babfb73ae4d35e9e3bf
aldohemsn <97897061+aldohemsn@users.noreply.github.com>
2024-07-26 11:06:44 +0800

    Update chat-setting-limits.ts

--- lib/chat-setting-limits.ts
+++ lib/chat-setting-limits.ts
@@ -126,7 +126,7 @@ export const CHAT_SETTING_LIMITS: Record<LLMID, ChatSettingLimits> = {
   },
 
   // OPENAI MODELS
-  "gpt-3.5-turbo": {
+  "gpt-4o-mini": {
     MIN_TEMPERATURE: 0.0,
     MAX_TEMPERATURE: 2.0,
     MAX_TOKEN_OUTPUT_LENGTH: 4096,
